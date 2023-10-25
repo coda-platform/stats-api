db69092fe2a52d8a867e5762f2df2e1759e4f4d9
Louis Mullie <louis.mullie@gmail.com>
2023-10-25 03:12:05 -0400

    Safety check

--- src/models/request/condition.ts
+++ src/models/request/condition.ts
@@ -3,26 +3,27 @@ import { ConditionOperator } from "./conditionOperator";
 
 export default interface Condition {
     conditionOperator: ConditionOperator;
-    conditions: Array<Condition|Filter>
+    conditions: Array<Condition | Filter>
 }
 
-function instanceOfCondition(object:any): object is Condition {
+function instanceOfCondition(object: any): object is Condition {
     return 'conditionOperator' in object;
 }
 
-function flattenConditionToFilters(condition:Condition): Filter[] {
-    if(!condition.conditions) return [];
-    return condition.conditions.reduce((result:Filter[], next) => {
-        if(instanceOfFilter(next)){
+function flattenConditionToFilters(condition: Condition): Filter[] {
+    if (!condition) return [];
+    if (!condition.conditions) return [];
+    return condition.conditions.reduce((result: Filter[], next) => {
+        if (instanceOfFilter(next)) {
             result.push(next);
         }
-        else{
+        else {
             result = result.concat(flattenConditionToFilters(next))
         }
         return result
     }, [])
 }
 
-export { 
+export {
     instanceOfCondition, flattenConditionToFilters
 }
