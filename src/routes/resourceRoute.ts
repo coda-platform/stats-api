2e14849b72f01cc43a1016933faabb01c4b0a7f5
Jeffrey Li <jeffreyli16@hotmail.com>
2023-11-22 13:55:33 -0500

    aidbox health route

--- src/routes/resourceRoute.ts
+++ src/routes/resourceRoute.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from 'express';
-
-import { getResourcesInfo } from '../services/resourceService';
+import { getAidboxInfo, getResourcesInfo } from '../services/resourceService';
 
 const router = Router();
 
@@ -14,5 +13,11 @@ router.get('/', (req: Request, res: Response) => {
             });
         });
 });
+
+router.get('/health', async (req: Request, res: Response)=> {
+    const result = await getAidboxInfo();
+    console.log(result)
+    res.send(result);
+})
 
 export default router;
